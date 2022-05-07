396b9fea097f9582bbd5d6d2bec7bfb369dfb200
Diego <dreynoso.adm@gmail.com>
2022-05-06 22:59:07 -0300

    se realiza desafio js 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,56 @@
-let turnosdisp = 7;
+let confirmation = confirm('Confirmo que soy mayor de 18 años');
 
-for(i=1; i<=turnosdisp; i++){
-    let cliente = prompt("Ingrese su nombre");
-    alert("Bienvenido " + cliente);
-    let reserva = confirm("¿Reserva turno?");
-    if(reserva){
-        alert("Reserva confirmada. Turno asignado AF" + i );
-    } else{
-        i--;
+if(confirmation){
+    let evento = parseInt(prompt('Seleccione el evento deseado \n 1 - Daddy Yankee \n 2 - Babasonicos \n 3 - Dua Lipa'));
+    
+    switch (evento){
+        case 1: 
+            let fecha1 = parseInt(prompt('Seleccione fecha \n 1 - 10/10/2022 \n 2 - 11/10/2022'));
+            fecha1==1 ? fecha1='10/10/2022' : fecha1='11/10/2022';
+            let cantidad1 = parseInt(prompt('Seleccione cantidad de entradas (Máximo de 6) \n Valor $12000'));
+            const evento1 = new Compra (1, 'Daddy Yankee', fecha1, 12000, cantidad1);
+            total(evento1);
+            break;
+
+        case 2:
+            let fecha2 = parseInt(prompt('Seleccione fecha \n 1 - 15/05/2022 \n 2 - 16/15/2022'));
+            fecha2==1 ? fecha2='15/05/2022' : fecha2='16/15/2022';
+            let cantidad2 = parseInt(prompt('Seleccione cantidad de entradas (Máximo de 6) \n Valor $7800'));
+            const evento2 = new Compra (2, 'Babasonicos', fecha2, 7800, cantidad2);
+            total(evento2);
+            break;
+
+        case 3:
+            let fecha3 = confirm('Seleccione fecha \n 1 - 07/08/2022');
+            fecha3=='07/08/2022';
+            let cantidad3 = parseInt(prompt('Seleccione cantidad de entradas (Máximo de 6) \n Valor $14300'));
+            const evento3 = new Compra (1, 'Dua Lipa', fecha3, 14300, cantidad3);
+            total(evento3);
+            break;
+
+        default: 
+            alert('Selección incorrecta');
+            break;
+    }
+
+} else{
+    alert('No esta permitido la venta a menores de edad');
+}
+
+function Compra (id, evento, fecha, precio, cantidad){
+    this.id = id;
+    this.evento = evento;
+    this.fecha = fecha;
+    this.precio = precio;
+    this.cantidad = cantidad;
+}
+
+function total (x){
+    let total = x.precio * x.cantidad;
+    let aceptacion = confirm('Evento: ' + x.evento + '\n Fecha: ' + x.fecha + '\n Cantidad entradas: ' + x.cantidad + '\n Valor Unidad: $' + x.precio + '\n TOTAL= $' + total + '\n ¿Estas seguro?');
+    if(aceptacion){
+        alert('Compra exitosa');
+    } else {
+        alert('Compra cancelada');
     }
 }
-alert("Turnos agotados");
